dc95abea9a336134c0699701c1e437ba592b1dd3
eshao961 <76065620+eshao961@users.noreply.github.com>
2020-12-17 00:08:46 -0800

    Update Mywork.py
    The first two questions worked out smoothly for me. I was able to use loops to find the numbers that I need based on the pattern of the numbers. I spent a lot of time on Question 3, because I do not know how to find prime numbers. There are no visible patterns to find prime numbers except that they can only be divided by 1 and themselves. It is even harder to find the largest prime factor. I finally came up with an idea to use loops and if else statement to test if n can be divided by prime numbers smaller than the square root of n. I am still not a hundred percent sure if question three works. I would like to discuss it with my peers in class.

--- Mywork.py
+++ Mywork.py
@@ -1,1 +1,68 @@
+Dec 17th 3:00-4:15pm(75min)
 
+import math
+
+#Question 1
+def three_five ():
+#Use for loop to find multiples of 3 or 5   
+    multiples = []
+    for i in range(100):
+        if i %3 == 0 or i %5 == 0:
+            multiples += [i]
+#Add the numbers to the list, and find the sum   
+    s = sum(multiples)
+
+    return s
+
+# print(three_five())
+
+#Question 2
+def fibonacci():
+    fibo = []
+#define the first two numbers of the fibo sequence
+    even_fibo = [1, 2]
+    first = 1
+    second = 2
+
+    stop = False
+#while loop to create the sequence by adding up neighbouring numbers
+    while stop == False:
+        third = first + second
+        fibo += [third]
+        first = second
+        second = third
+#Stop the loop
+        if third > 4000000:
+            stop = True
+
+#Find even valued terms in the sequence
+    for num in fibo:
+        if num %2 == 0:
+            even_fibo += [num]
+        
+    s = sum(even_fibo)
+    return s
+
+# print(fibonacci())    
+
+#Question 3
+def prime():
+#enter the number
+    n = 600851475143
+    maxPrime = -1
+
+    while n % 2 == 0: 
+        maxPrime = 2
+        n >>= 1   
+#Divide n with different numbers to find the prime
+    for i in range(3, int(math.sqrt(n)) + 1, 2): 
+
+        while n % i == 0: 
+            maxPrime = i 
+            n = n / i 
+
+    if n > 2: 
+        maxPrime = n 
+    return int(maxPrime)
+
+print(prime())
